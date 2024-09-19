242b39cc932013e2aa6130fbe83ca826a50b9d2f
Zhanna Artyshova <zhanna.artyshova@buniq.it>
2024-09-19 11:08:04 +0200

    add and style CurrentInfo component

--- screens/DetailScreen.tsx
+++ screens/DetailScreen.tsx
@@ -6,11 +6,14 @@ import {
   Text,
   Pressable,
   Button,
+  StyleSheet,
+  Image,
 } from 'react-native';
 
 import {fetchWeather} from './ListScreen';
 import {Weather} from '../models/Weather';
 import createWeather from '../models/Weather';
+
 
 interface DetailScreenProps {
   city: string;
@@ -18,53 +21,27 @@ interface DetailScreenProps {
 }
 
 export default function DetailScreen({city, onPress}: DetailScreenProps) {
-
+  const [weather, setWeather] = useState<Weather>();
   const apiKey = '16f82f59dec74ab3be8140412241809';
 
-  const [weather, setWeather] = useState<Weather>();
-
-  //console.log(city);
 
   useEffect(() => {
     (async () => {
-      // console.log('hi1');
       const weatherData = await fetchWeather(apiKey, city, 10);
-      //console.log('hi2', weatherData);
-
       const newWeather = createWeather(weatherData, true);
-      console.log('hi3', newWeather);
-
       setWeather(newWeather);
-      //console.log('hi4', weather);
     })();
   }, []);
 
-  // const weatherData = fetchWeather(apiKey, city, 10)
-  // const newWeather = createWeather(weatherData, true)
-
-  // setWeather(newWeather)
-
-  // useEffect(() => {
-  //   const weatherGeneral = fetchWeather(api, apiKey, city)
-  //   const weatherHourly = fetchWeather(apiHourly, apiKey, city)
-  //   const weatherDaily = fetchWeather(apiDaily, apiKey, city, "10")
-
-  //   const newWeather = createWeather(weatherGeneral, weatherHourly, weatherDaily)
-
-  //   setWeather(newWeather)
-
-  // }, [])
 
   return (
     <View>
-      <Text>DetailScreen </Text>
-
+      
       <CurrentInfo weather={weather} />
 
       <HourlyForecast />
 
       <DailyForecast />
-      <Text>{city}</Text>
 
       <GeneralInfo />
 
@@ -73,12 +50,40 @@ export default function DetailScreen({city, onPress}: DetailScreenProps) {
   );
 }
 
-function CurrentInfo(weather: Weather) {
-  return (
-    <View>
-      <Text> KK</Text>
+interface CurrentInfoProps{
+  weather: Weather | undefined
+}
+
+
+
+function CurrentInfo({weather}: CurrentInfoProps) {
+  if (weather){
+   
+
+    return (
+    <View style={styles.currentContainer}>
+    
+      <Image source={{uri: `https:${weather.icon}`}}  style={{ width: 64, height: 64 }}></Image>
+      <Text style={styles.currentCity}>{weather.city}</Text>
+
+      <Text style={styles.currentTemp}>{weather.temp}°</Text>
+
+      <Text>{weather.description}</Text>
+ 
+      <Text>H:{weather.temp_min}° L:{weather.temp_max}°</Text>
+      
+      
     </View>
+    
   );
+  } else{
+    return (
+      <View>
+        <Text>Add more weather details</Text>
+      </View>
+    )
+  }
+  
 }
 
 function HourlyForecast() {
@@ -92,3 +97,18 @@ function DailyForecast() {
 function GeneralInfo() {
   return <View></View>;
 }
+
+
+const styles = StyleSheet.create({
+  currentContainer:{
+    alignItems: 'center',
+  },
+  currentCity:{
+    fontSize:20,
+    fontWeight: 'bold'
+  },
+  currentTemp:{
+    fontSize:60
+  }
+
+})
--- screens/ListScreen.tsx
+++ screens/ListScreen.tsx
@@ -14,12 +14,10 @@ import DetailScreen from './DetailScreen';
 import {Weather} from '../models/Weather';
 import createWeather from '../models/Weather';
 
-import useFetchWeather from '../useFetchWeather';
-
 var CITIES = require('../CITIES.json');
 
+
 export async function fetchWeather(apiKey: string, city: string, days: number) {
-  //const [data, setData] = useState(null);
   const url = `https://api.weatherapi.com/v1/forecast.json?days=${days.toString()}&key=${apiKey}&q=${city}&aqi=no&alerts=no`;
 
   try {
@@ -38,14 +36,12 @@ export async function fetchWeather(apiKey: string, city: string, days: number) {
 export default function ListScreen() {
   const [data, setData] = useState(CITIES);
   const [fullData, setFullData] = useState(CITIES);
-  //const [weatherData, setWeatherData] = useState<any>({});
   const [selectedCity, setSelectedCity] = useState('');
   const [weatherList, setWeatherList] = useState<Weather[]>([]);
   const [filterText, setFilterText] = useState('');
-  //const [isFound, setIsFound] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
-  // const api = 'https://api.openweathermap.org/data/2.5/weather?q=';
+
   const apiKey = '16f82f59dec74ab3be8140412241809';
 
   function isCityInList(city: string, arr: Weather[]) {
@@ -54,19 +50,6 @@ export default function ListScreen() {
 
   function addToList(weatherData: any) {
     try {
-      // const newWeather: Weather = {
-      //   city: weatherData.name,
-      //   description: weatherData.weather[0].description,
-      //   temp: weatherData.main.temp,
-      //   feels_like: weatherData.main.feels_like,
-      //   temp_min: weatherData.main.temp_min,
-      //   temp_max: weatherData.main.temp_max,
-      //   pressure: weatherData.main.pessure,
-      //   humidity: weatherData.main.humidity,
-      //   visibility: weatherData.visibility,
-      //   sunrise: weatherData.sys.sunrise,
-      //   sunset: weatherData.sys.sunset,
-      // };
 
       const newWeather = createWeather(weatherData, false);
 
@@ -84,7 +67,6 @@ export default function ListScreen() {
         const weatherData = await fetchWeather(apiKey, city, 1);
 
         addToList(weatherData);
-        // sconsole.log(weatherData,  selectedCity, city);
       }
     } catch (error) {
       console.error('Failed to add the city', error);
@@ -118,7 +100,6 @@ export default function ListScreen() {
       </View>
     );
   } else {
-    // console.log(isOpen, 0, selectedCity);
     return (
       <View>
         <Text>Weather</Text>
