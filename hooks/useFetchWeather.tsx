4123b5c23d0f5a8b8cb24f8f733da583e79041dc
Zhanna Artyshova <zhanna.artyshova@buniq.it>
2024-10-08 14:29:16 +0200

    update useFetchWeather hook: optimized with Promise.all()

--- hooks/useFetchWeather.tsx
+++ hooks/useFetchWeather.tsx
@@ -7,29 +7,28 @@ const useFetchWeather = (city: string[] | string, days: number) => {
   const [data, setData] = useState<Weather[]>();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  let hasForecast = false;
 
   useEffect(() => {
     const fetchWeather = async () => {
       setLoading(true);
       try {
         const cities = Array.isArray(city) ? city : [city];
-        const weatherList: Weather[] = [];
 
-        for (const currenCity of cities) {
-          const url = `https://api.weatherapi.com/v1/forecast.json?days=${days.toString()}&key=${API_KEY}&q=${currenCity}&aqi=no&alerts=no`;
+        const fetchPromises = cities.map(async (currentCity) => {
+          const url = `https://api.weatherapi.com/v1/forecast.json?days=${days.toString()}&key=${API_KEY}&q=${currentCity}&aqi=no&alerts=no`;
           const response = await fetch(url);
           if (!response.ok) {
             // error coming back from server
             throw new Error('Could not fetch the data for that resource');
           }
           const result = await response.json();
-          days > 1 ? (hasForecast = true) : null;
-          const weather = createWeather(result, hasForecast);
-          weatherList.push(weather);
-        }
+          const hasForecast = days > 1;
+          return createWeather(result, hasForecast);
+        })
 
+        const weatherList: Weather[] = await Promise.all(fetchPromises)
         setData(weatherList);
+        
       } catch (err: any) {
         setError(err.message);
       } finally {
